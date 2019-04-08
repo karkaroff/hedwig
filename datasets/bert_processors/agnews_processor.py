4fbe5fc18e12d1bd1e7efd0b25e3434bac0c6808
Ashutosh-Adhikari <aashu.ad@gmail.com>
2019-04-07 22:28:26 -0400

    Add support for AAPD, Sogou, AGNews and Yelp2014

--- datasets/bert_processors/agnews_processor.py
+++ datasets/bert_processors/agnews_processor.py
@@ -3,24 +3,25 @@ import os
 from datasets.bert_processors.abstract_processor import BertProcessor, InputExample
 
 
-class AAPDProcessor(BertProcessor):
-    NAME = 'AAPD'
-    NUM_CLASSES = 54
-    IS_MULTILABEL = True
+class AGNewsProcessor(BertProcessor):
+    NAME = 'AGNews'
+    NUM_CLASSES = 4
+    IS_MULTILABEL = False
 
     def get_train_examples(self, data_dir):
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir,'AAPD', 'train.tsv')), 'train')
+            self._read_tsv(os.path.join(data_dir, 'AGNews', 'train.tsv')), 'train')
 
     def get_dev_examples(self, data_dir):
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, 'AAPD', 'dev.tsv')), 'dev')
+            self._read_tsv(os.path.join(data_dir, 'AGNews', 'dev.tsv')), 'dev')
 
     def get_test_examples(self, data_dir):
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, 'AAPD', 'test.tsv')), 'test')
+            self._read_tsv(os.path.join(data_dir, 'AGNews', 'test.tsv')), 'test')
 
     def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
         examples = []
         for (i, line) in enumerate(lines):
             if i == 0:
